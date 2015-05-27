23be6c2862f252da73cc114c9f4623bfc85a33ff
ashishMMM <aupadhyay.id@gmail.com>
2015-05-27 12:11:57 +0530

    path
    link updated

--- WebContent/js/addjquery.js
+++ WebContent/js/addjquery.js
@@ -65,4 +65,14 @@ $('#contact').click(function(){
 	 $('html, body').animate({scrollTop: offset.top}, 'slow');
 	return false;
 }); 
+$('#healthok').click(function(){
+	$(this).addClass('active');
+	 $('html, body').animate({scrollTop: 0}, 'slow');
+	return false;
+}); 
+$('#home').click(function(){
+	 $('html, body').animate({scrollTop: 0}, 'slow');
+	return false;
+}); 
+
 });
